cb06c45cec87bdbae020f5aaafe0004a117e574a
leetuandat <tuandat24034@gmail.com>
2026-05-29 23:25:33 +0700

    Attempted 1 more today

--- Easy/E577_Employee Bonus.sql
+++ Easy/E577_Employee Bonus.sql
@@ -10,6 +10,11 @@
 
 
 /* --- MY SOLUTION --- */
-SELECT  FROM
-    
-;
+SELECT 
+    e.name,
+    b.bonus
+FROM Employee e
+LEFT JOIN Bonus b
+    ON e.empId = b.empId
+WHERE b.bonus < 1000
+   OR b.bonus IS NULL;
